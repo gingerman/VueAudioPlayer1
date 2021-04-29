027d1bd3edcdad82b4bc313c6ee3bff4ce659a95
gingerman <steve@gingerman.co.uk>
2021-04-29 17:51:40 +0100

    Arrrgh work

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,13 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  
+  base:"/",
+  publicDir:"public/"
 })
 
 
 // base:"/brozone_zone_1/dist/",
+
+
+// require:"./audio/",
+// publicDir:"./audio/",
