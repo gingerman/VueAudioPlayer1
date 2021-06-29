317e1778e2a4c40721b6c406395699b84aac963e
gingerman <steve@gingerman.co.uk>
2021-06-29 03:22:34 +0100

    updated so that json file is loaded dynamically

--- src/App.vue
+++ src/App.vue
@@ -6,33 +6,66 @@
 </template>
 
 <script>
-import AudioAppContainer from './components/AudioAppContainer.vue'
-import Tracks from './components/Tracks.vue'
-import tracksJson from './data/data.json'
+	import AudioAppContainer from './components/AudioAppContainer.vue'
+	import Tracks from './components/Tracks.vue'
+	// import tracksJson from './data/data.json'
+	import axios from 'axios'
 
-export default {
-  name: 'App',
-  data() {
-    return{
-      tracks: tracksJson
-    }
-  },
-  props: {
-    title: String,
-  },
-  components:{
-    AudioAppContainer, Tracks
-  },
-  created() {
+	const tracks = [];
+
+	export default{
+		data() {
+			return{
+				tracks: tracks,
+			}
+		},
+		props: {
+    		title: String,
+  		},
+		components:{
+    		AudioAppContainer, Tracks
+  		},
+  		mounted() {
     
-  }
-  	//   mounted(){
-	// 	getJSON('./assets/data.json', json => {
-	//     this.tracks = json
-	//   })
-	//   },
-}
+  		},
+		created(){
 
+			//console.log( "mounted >> this = " + tracks );
+
+			axios(
+					{
+					method: 'get',
+					url: './data.json',
+					responseType: 'json'
+					}
+				)
+				.then(
+
+					response => (this.tracks = response.data)
+
+					// this.tracks = function (response) {
+					// 	console.log("RESPONSE\n++++++++++\n");//+response.data);
+					// 	console.log(response.status);
+					// 	console.log(response.statusText);
+					// 	console.log(response.headers);
+					// 	console.log(response.config);
+
+					// 	if ( response.data.length > 0 ){
+
+					// 		console.log("if RESPONSE length ++++++++++ Data Length "+response.data.length );
+					// 		//console.log( "this = " + this.data );
+					// 		return response.data;
+
+					// 	}
+					// }
+
+					
+					//response => console.log( "\n\nRESPONSE\n+AAA+++++++++\n" + JSON.stringify(  response.data) ),
+					//response => this.data.tracks = JSON.stringify( response.data )[0],
+					//console.log( "\n\nRESPONSE\n+BBB+++++++++\n" + this.tracks ),
+				)
+			}
+		}
 </script>
 
 <style>
